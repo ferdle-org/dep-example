63bb96f7f6d467c0eb133e7815925393d634125a
Fernando Ruiz Diaz <ferdlestier@github.com>
2023-04-18 15:52:56 -0400

    adding on testing vulnerability

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+group = "au.com.adatree.tpi.mogoplus"
+
+allprojects {
+    group = "au.com.adatree.tpi.mogoplus"
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
